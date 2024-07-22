9d7de80aae08c2ff56199523c3004f6b485c1b8e
Devon Sasaki <dsasaki3@gatech.edu>
2024-07-21 21:56:13 -1000

    Update app.js

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ app.get('/version', (req, res) => {
   res.send('10')
 })
 
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/dist/index.html')
-})
-
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
