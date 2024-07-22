4246a228ce92d81a2fbe93dbb98b4746f884edb4
Devon Sasaki <dsasaki3@gatech.edu>
2024-07-21 21:26:42 -1000

    Update pokedex.cy.js

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,13 +4,13 @@
 
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
   it('pokemon page can be accessed', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:80800')
     cy.contains('ivysaur').click()
     cy.url().should('include', '/pokemon/ivysaur')
     cy.contains('chlorophyll')
